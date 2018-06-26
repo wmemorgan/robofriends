2bc19fd3cf3bcd3e34d2e05f2fe465ae14a1d616
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-06-26 11:40:37 +0800

    Add Error Boundary

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasErrors: false
+    }
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasErrors: true })
+  }
+
+  render() {
+    if (this.state.hasErrors) {
+      return <h1>Oooops! That is not good.</h1>
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundry;
